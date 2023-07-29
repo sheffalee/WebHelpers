f0d00fa1fb8d3186b3d2cbeec33ace92c1bf6652
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-07-29 18:51:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,9 @@
 import streamlit as st
-# import pickle
+import cloudpickle as cp
 
-# # Load the sentiment analysis function from the pickle file
-# with open('sentiment_analysis_model.pkl', 'rb') as file:
-#     get_sentiment_label = pickle.load(file)
-import dill
-
-# Load the sentiment analysis function from the dill file
+# Load the sentiment analysis function from the cloudpickle file
 with open('sentiment_analysis_model.pkl', 'rb') as file:
-    get_sentiment_label = dill.load(file)
+    get_sentiment_label = cp.load(file)
 
 def main():
     # Set a title for the app
@@ -27,3 +22,33 @@ def main():
 
 if __name__ == '__main__':
     main()
+
+# import streamlit as st
+# # import pickle
+
+# # # Load the sentiment analysis function from the pickle file
+# # with open('sentiment_analysis_model.pkl', 'rb') as file:
+# #     get_sentiment_label = pickle.load(file)
+# import dill
+
+# # Load the sentiment analysis function from the dill file
+# with open('sentiment_analysis_model.pkl', 'rb') as file:
+#     get_sentiment_label = dill.load(file)
+
+# def main():
+#     # Set a title for the app
+#     st.title("Sentiment Analysis Web App")
+
+#     # Get input text from the user
+#     user_input = st.text_area("Please enter your sentence:")
+
+#     # Check if the user has entered any text
+#     if user_input:
+#         # Call the sentiment analysis function to get the sentiment label
+#         sentiment_label = get_sentiment_label(user_input)
+
+#         # Display the sentiment label
+#         st.write("Sentiment:", sentiment_label)
+
+# if __name__ == '__main__':
+#     main()
