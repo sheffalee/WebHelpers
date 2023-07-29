6d3ef947655c5ba4f99745617a5680e6a306bc5d
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-07-29 19:13:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,54 @@
 import streamlit as st
 import pickle
 from sentiment_analysis import get_sentiment_label  # Import the function from sentiment_analysis.py
+from chatbot import chatbot_response  # Import the chatbot_response function from chatbot.py
 
 def main():
     # Set a title for the app
-    st.title("Sentiment Analysis Web App")
+    st.title("Sentiment Analysis and Chatbot Web App")
 
     # Get input text from the user
     user_input = st.text_area("Please enter your sentence:")
 
     # Check if the user has entered any text
     if user_input:
-        # Call the sentiment analysis function to get the sentiment label
+        # Perform sentiment analysis and get the sentiment label
         sentiment_label = get_sentiment_label(user_input)
 
         # Display the sentiment label
         st.write("Sentiment:", sentiment_label)
 
+        # Prepare messages for chatbot
+        messages = [{"role": "user", "content": user_input}]
+        chatbot_reply = chatbot_response(messages)
+
+        # Display the chatbot response
+        st.markdown("Chatbot: " + chatbot_reply)
+
 if __name__ == '__main__':
     main()
+
+# import streamlit as st
+# import pickle
+# from sentiment_analysis import get_sentiment_label  # Import the function from sentiment_analysis.py
+
+# def main():
+#     # Set a title for the app
+#     st.title("Sentiment Analysis Web App")
+
+#     # Get input text from the user
+#     user_input = st.text_area("Please enter your sentence:")
+
+#     # Check if the user has entered any text
+#     if user_input:
+#         # Call the sentiment analysis function to get the sentiment label
+#         sentiment_label = get_sentiment_label(user_input)
+
+#         # Display the sentiment label
+#         st.write("Sentiment:", sentiment_label)
+
+# if __name__ == '__main__':
+#     main()
 
 # import streamlit as st
 # import cloudpickle as cp
