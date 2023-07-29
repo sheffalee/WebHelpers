6cc63dd224762b56e3378ab910340162287d7b4f
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-07-29 18:52:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import cloudpickle as cp
-
-# Load the sentiment analysis function from the cloudpickle file
-with open('sentiment_analysis_model.pkl', 'rb') as file:
-    get_sentiment_label = cp.load(file)
+import pickle
+from sentiment_analysis import get_sentiment_label  # Import the function from sentiment_analysis.py
 
 def main():
     # Set a title for the app
@@ -24,16 +21,11 @@ if __name__ == '__main__':
     main()
 
 # import streamlit as st
-# # import pickle
+# import cloudpickle as cp
 
-# # # Load the sentiment analysis function from the pickle file
-# # with open('sentiment_analysis_model.pkl', 'rb') as file:
-# #     get_sentiment_label = pickle.load(file)
-# import dill
-
-# # Load the sentiment analysis function from the dill file
+# # Load the sentiment analysis function from the cloudpickle file
 # with open('sentiment_analysis_model.pkl', 'rb') as file:
-#     get_sentiment_label = dill.load(file)
+#     get_sentiment_label = cp.load(file)
 
 # def main():
 #     # Set a title for the app
@@ -52,3 +44,33 @@ if __name__ == '__main__':
 
 # if __name__ == '__main__':
 #     main()
+
+# # import streamlit as st
+# # # import pickle
+
+# # # # Load the sentiment analysis function from the pickle file
+# # # with open('sentiment_analysis_model.pkl', 'rb') as file:
+# # #     get_sentiment_label = pickle.load(file)
+# # import dill
+
+# # # Load the sentiment analysis function from the dill file
+# # with open('sentiment_analysis_model.pkl', 'rb') as file:
+# #     get_sentiment_label = dill.load(file)
+
+# # def main():
+# #     # Set a title for the app
+# #     st.title("Sentiment Analysis Web App")
+
+# #     # Get input text from the user
+# #     user_input = st.text_area("Please enter your sentence:")
+
+# #     # Check if the user has entered any text
+# #     if user_input:
+# #         # Call the sentiment analysis function to get the sentiment label
+# #         sentiment_label = get_sentiment_label(user_input)
+
+# #         # Display the sentiment label
+# #         st.write("Sentiment:", sentiment_label)
+
+# # if __name__ == '__main__':
+# #     main()
