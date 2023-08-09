8e7c4ec77d12d8200f35f177efd9800ef60fd99c
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-08-09 17:55:45 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -2,6 +2,7 @@ import openai
 import streamlit as st
 from streamlit_chat import message
 
+api_secret = "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"
 openai.api_key = st.secrets["api_secret"]
 
 def generate_response(prompt):
