0a569d2e423576d9792948f3dfbc99c48044ea70
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-08-09 17:14:08 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,1 @@
-import openai
 
-openai.api_key = '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'
-
-def chatbot_response(messages):
-    user_input = messages[-1]["content"]
-
-    if user_input.lower() == "bye":
-        return "Goodbye! Have a great day."
-
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
-    return response["choices"][0]["message"]["content"]
