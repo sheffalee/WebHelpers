2caceac04c47e869939eb9dc6a70854849cc96ad
Sheffalee <87572002+sheffalee@users.noreply.github.com>
2023-08-18 22:47:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # Import the required libraries
 import openai
 import streamlit as st
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoConfig
+
 
 headers={
     "authorization":st.secrets["API_KEY"],
@@ -10,6 +12,33 @@ headers={
 #Set the openai api key
 openai.api_key = st.secrets["API_KEY"]
 
+
+# Define a function to preprocess text
+def preprocess(text):
+    new_text = []
+    for t in text.split(" "):
+        t = '@user' if t.startswith('@') and len(t) > 1 else t
+        t = 'http' if t.startswith('http') else t
+        new_text.append(t)
+    return " ".join(new_text)
+
+# Define sentiment analysis model and tokenizer
+MODEL = "cardiffnlp/twitter-roberta-base-sentiment-latest"
+tokenizer = AutoTokenizer.from_pretrained(MODEL)
+config = AutoConfig.from_pretrained(MODEL)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+# Define a function to predict sentiment
+def predict_sentiment(text):
+    text = preprocess(text)
+    encoded_input = tokenizer(text, return_tensors='pt')
+    output = model(**encoded_input)
+    scores = output[0][0].detach().numpy()
+    scores = softmax(scores)
+    ranking = np.argsort(scores)
+    highest_sentiment = config.id2label[ranking[-1]]
+    return highest_sentiment
+    
 # Initialize session state
 if 'chat_history' not in st.session_state:
     st.session_state.chat_history = []
